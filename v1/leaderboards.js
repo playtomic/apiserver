595e5924e1d22ccd359f1936fd0d07db1d9e1803
Ben Lowry <benlowry@outlook.com>
2013-05-12 21:12:01 -0600

    Method name is case-sensitive

--- v1/leaderboards.js
+++ v1/leaderboards.js
@@ -1,5 +1,5 @@
 module.exports = {
     list: require(__dirname + "/leaderboards.list.js"),
     save: require(__dirname + "/leaderboards.save.js"),
-    saveAndList: require(__dirname + "/leaderboards.saveandlist.js")
+    saveandlist: require(__dirname + "/leaderboards.saveandlist.js")
 };
