5af8d18e715b3d858a3659ea82629027864c5819
Ben Lowry <benlowry@outlook.com>
2013-05-16 15:05:49 -0600

    Added achievements collections

--- api/database.js
+++ api/database.js
@@ -7,6 +7,6 @@ db.poolEnabled = true;
 db.poolSize = 20;
 db.killEnabled = true;
 db.setDatabases(config.mongo);
-db.playtomic.collections(["games", "gamevars", "leaderboard_scores", "leaderboard_tables", "leaderboard_bans", "playerlevel_levels", "playerlevel_bans"]);
+db.playtomic.collections(["games", "gamevars", "leaderboard_scores", "leaderboard_tables", "leaderboard_bans", "playerlevel_levels", "playerlevel_bans", "achievements", "achievements_player"]);
 
 module.exports = db;
